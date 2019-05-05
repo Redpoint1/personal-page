d56c3dce6db2bec26cc98053055bdb0a0f89cf72
Redpoint1 <Redpoints1@gmail.com>
2019-05-05 17:46:13 +0200

    build: move custom preset to new repo

--- utils/preset/parser-opts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  headerPattern: /^(?::(\w*): )?(?:(\w*)(?:\(([\w$.\-* ]*)\))?: )?(.*)$/,
-  headerCorrespondence: ["emoji", "type", "scope", "subject"],
-  noteKeywords: [`NOTE`],
-  revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
-  revertCorrespondence: [`header`, `hash`]
-};
