449e86b66e09439ff0c32b9108e7852f1654fc1e
Redpoint1 <Redpoints1@gmail.com>
2019-05-06 00:25:26 +0200

    build: add vue-svg-inline dependency

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,24 @@ module.exports = {
   css: {
     sourceMap: true
   },
-  assetsDir: "static"
+  assetsDir: "static",
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule
+      .oneOf("inline")
+      .resourceQuery(/inline/)
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader")
+      .end()
+      .end()
+      .oneOf("external")
+      .use("file-loader")
+      .loader("file-loader")
+      .options({
+        name: "assets/[name].[hash:8].[ext]"
+      });
+  }
 };
