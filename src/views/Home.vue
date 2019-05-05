4a456254a19d242c451e83a763beba03169f590e
Redpoint1 <Redpoints1@gmail.com>
2019-05-06 00:26:51 +0200

    perf(svg): inline brand svgs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,28 +3,41 @@
     <div class="row">
       <div class="col d-flex justify-content-around">
         <a href="https://github.com/Redpoint1">
-          <img
-            class="brand"
-            src="~@fortawesome/fontawesome-free/svgs/brands/github.svg"
-          />
+          <Github class="brand" />
         </a>
       </div>
       <div class="col d-flex justify-content-around">
         <a href="https://www.linkedin.com/in/richardrozar/">
-          <img
-            class="brand"
-            src="~@fortawesome/fontawesome-free/svgs/brands/linkedin.svg"
-          />
+          <LinkedIn class="brand" />
         </a>
       </div>
     </div>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import Github from "@/assets/github.svg?inline";
+import LinkedIn from "@/assets/linkedin.svg?inline";
+
+@Component({
+  components: {
+    Github,
+    LinkedIn
+  }
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
 .brand {
   height: 25vw;
   min-height: 100px;
   max-height: 200px;
+
+  &:hover {
+    fill: red;
+  }
 }
 </style>
