063a0bf11b82597088baf6e7e54c0607138cfd2e
Redpoint1 <Redpoints1@gmail.com>
2019-05-08 15:06:35 +0200

    change(js): add .active for menu item by vue-router

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand navbar-light">
     <div class="container">
       <ul class="navbar-nav">
-        <li class="nav-item active">
+        <li class="nav-item">
           <router-link to="/" class="nav-link">Home</router-link>
         </li>
         <li class="nav-item">
